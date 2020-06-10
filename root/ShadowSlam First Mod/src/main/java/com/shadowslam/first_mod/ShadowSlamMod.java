2e40222a61900378e93b620310630574b74e2c87
Shad0wSlam <28639548+Shad0wSlam@users.noreply.github.com>
2020-06-10 17:09:33 +0100

    Add files via upload

--- root/ShadowSlam First Mod/src/main/java/com/shadowslam/first_mod/ShadowSlamMod.java
+++ root/ShadowSlam First Mod/src/main/java/com/shadowslam/first_mod/ShadowSlamMod.java
@@ -1,7 +1,9 @@
 package com.shadowslam.first_mod;
 
+import net.minecraftforge.fml.common.Mod;
+
 @Mod(ShadowSlamMod.shadowslam_first)
 public class ShadowSlamMod {
 
-	public static final String shadowslam_first = "shadowslam_mod"
+	public static final String shadowslam_first = "shadowslam_mod";
 }
